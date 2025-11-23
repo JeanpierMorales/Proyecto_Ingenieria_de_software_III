3e1293f0d1046f8600da6a3e9f4cdb54cddee165
Jeanpier Morales <omar.morales@usil.pe>
2025-11-23 01:13:09 -0500

    parse

--- src/components/BudgetForm.jsx
+++ src/components/BudgetForm.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
 import { Plus, X } from "lucide-react";
 import { validateBudget } from "../utils/validations";
 import { projectsAPI } from "../services/api";
